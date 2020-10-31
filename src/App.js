6375661c864dbafea2ee82c95071a3afc347cca9
zeref10 <yashchaturvedi296@gmail.com>
2020-10-31 16:17:30 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  // BEM naming convention
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDAGQNaBWfiYUEWRaSerW2Qr0hNhdIbT28",
+    authDomain: "whatsapp-clone-24fb5.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-24fb5.firebaseio.com",
+    projectId: "whatsapp-clone-24fb5",
+    storageBucket: "whatsapp-clone-24fb5.appspot.com",
+    messagingSenderId: "463241103343",
+    appId: "1:463241103343:web:afe56a49e901300d21d81a"
+  };
